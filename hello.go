c571fe502708a04fa7948bf3824ed14c4e8c444a
val8828 <val8828@yandex.ru>
2019-10-24 23:51:30 +0400

    first ver. todo set workers count

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ import (
    	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"os"
-//	"sync"
+	"time"
     "flag"   
     "strings"
   	"io"
@@ -19,63 +19,88 @@ var (
 	pagesWithFiles map[string]string	
 	albumName string
 	WORKERS int = 2 //кол-во "потоков"
+	url string
 )
 
 
-func _check(err error) {
+func _check(doc *goquery.Document, err error) int {
 	if err != nil {
 		panic(err)
 	}
+	if strings.Contains(doc.Find("title").Text(), "Error") {		
+		return 404		
+	}
+	return 0
 }
 
-// основная функция обработки
-func parseUrl(url string) {
+//fmt.Println("request: " + url)
 
-	//fmt.Println("request: " + url)
-	
+// основная функция обработки
+func parseUrl(url string, level int )  {	
 	// заворачиваем источник в goquery документ
 	doc, err := goquery.NewDocument(url)
 
-	_check(err)
-
-	// в манере jquery, css селектором получаем все ссылки
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-		if val, ok := s.Attr("href"); ok {
-			//Check if url ends with mp3
-			if strings.HasSuffix(val, "mp3") {
-				val := "https://downloads.khinsider.com" + val
-				// put all url in map (only first in)
-				if _,ok :=  pagesWithFiles[val]; !ok {
-					pagesWithFiles[val] = s.Text()					
-				}
+	switch level{
+	case 1:
+		if _check(doc, err)	== 0 {
+			//get album name from first <h2> tag
+			if(albumName == ""){
+				doc.Find("h2").EachWithBreak(func(i int, s *goquery.Selection) bool {		
+					albumName =  s.Text()
+					return false
+				}) 
 			}
-	    }
-	})
-	//get album nane from first <h2> tag
-	doc.Find("h2").EachWithBreak(func(i int, s *goquery.Selection) bool {		
-		albumName =  s.Text()
-		return false
-	}) 	
+			// в манере jquery, css селектором получаем все ссылки
+			doc.Find("a").Each(func(i int, s *goquery.Selection) {
+				if val, ok := s.Attr("href"); ok {
+					//Check if url ends with mp3
+					if strings.HasSuffix(val, "mp3") {		
+						val := "https://downloads.khinsider.com" + val						
+						parseUrl(val, 2)
+					}
+	    		}
+			})
+		}
+
+	case 2:				
+		// в манере jquery, css селектором получаем все ссылки
+		doc.Find("a").Each(func(i int, s *goquery.Selection) {			
+			if val, ok := s.Attr("href"); ok {				
+				//Check if url ends with mp3
+				if strings.HasSuffix(val, "mp3") {								
+					// put all url in map (only first in)
+					if _,ok :=  pagesWithFiles[val]; !ok {						
+						pagesWithFiles[val] = findSongName(doc)					
+					}
+				}
+    		}
+		})
+	}	
 }
 
-func downloadFilesCNTRL() {
-	//var countOfWorkers int
-	//var alreadyDoneFiles int
-	
-	//keys := make([]string, 0, len(pagesWithFiles))
-    /*for k := range pagesWithFiles {
+func findSongName(doc *goquery.Document) string {
+	var songName string
+	doc.Find("b").Each(func(i int, s *goquery.Selection) {			
+			if i == 3 {			
+				songName = s.Text()
+			}
+		})
+	return songName
+}
 
-
-        keys = append(keys, k)    
-    }
-
-
-*/
+func downloadFilesCNTRL() {	
+	os.MkdirAll(albumName,0777)
 	for k, v := range pagesWithFiles {
-		//go func() { 
-			fmt.Println("key", k , "value", v )
-		//}()
+		go func(i string, j string) { 			
+			if err := DownloadFile(albumName + "\\" + j + ".mp3", i); err != nil {
+        		panic(err)
+        		fmt.Println(err)
+    		}
+		}(k , v)
+
 	}
+		time.Sleep(20 *time.Second)		
+    	fmt.Println("The End")
 	//(countOfWorkers <= WORKERS) && (alreadyDoneFiles < len(pagesWithFiles)){
 //		currentLink := 
 	
@@ -105,8 +130,9 @@ func DownloadFile(filepath string, url string) error {
 }
 
 func initial() {
-	flag.StringVar(&searchCombination,"sc", "zelda", "страница с музакальным альбомом")	
+	flag.StringVar(&url,"url", "https://downloads.khinsider.com/game-soundtracks/album/death-brade", "страница с музакальным альбомом")	
 	flag.IntVar(&WORKERS, "w", WORKERS, "количество потоков")
+	flag.StringVar(&albumName,"an", "", "путь папки")	
 
 	pagesWithFiles = make(map[string]string)
 }
@@ -115,11 +141,9 @@ func main() {
 	
 	initial()
 
-	flag.Parse()		
+	flag.Parse()			
 
-	url := "https://downloads.khinsider.com/game-soundtracks/album/adk-world"
-
-	parseUrl(url)		
+	parseUrl(url,1)		
 	
 	downloadFilesCNTRL()
 }
