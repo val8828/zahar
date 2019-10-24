d5811b3bf4c24a5c48b6dbc5d90ff47c37da660f
val8828 <val8828@yandex.ru>
2019-10-24 18:20:15 +0300

    todo goroutine for download

--- hello.go
+++ hello.go
@@ -5,16 +5,20 @@ package main
 import (
    	"fmt"
 	"github.com/PuerkitoBio/goquery"
-//	"os"
-	"sync"
+	"os"
+//	"sync"
     "flag"   
     "strings"
-  
+  	"io"
+  	"net/http"
+
 )
 
 var (
 	searchCombination string // 
-	m map[string]string
+	pagesWithFiles map[string]string	
+	albumName string
+	WORKERS int = 2 //кол-во "потоков"
 )
 
 
@@ -28,7 +32,7 @@ func _check(err error) {
 func parseUrl(url string) {
 
 	//fmt.Println("request: " + url)
-
+	
 	// заворачиваем источник в goquery документ
 	doc, err := goquery.NewDocument(url)
 
@@ -36,44 +40,87 @@ func parseUrl(url string) {
 
 	// в манере jquery, css селектором получаем все ссылки
 	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-	if val, ok := s.Attr("href"); ok {
+		if val, ok := s.Attr("href"); ok {
+			//Check if url ends with mp3
 			if strings.HasSuffix(val, "mp3") {
-				if _,ok :=  m[val]; !ok {
-					m[val] = s.Text()					
+				val := "https://downloads.khinsider.com" + val
+				// put all url in map (only first in)
+				if _,ok :=  pagesWithFiles[val]; !ok {
+					pagesWithFiles[val] = s.Text()					
 				}
-				
 			}
-        }
-	})		
+	    }
+	})
+	//get album nane from first <h2> tag
+	doc.Find("h2").EachWithBreak(func(i int, s *goquery.Selection) bool {		
+		albumName =  s.Text()
+		return false
+	}) 	
+}
+
+func downloadFilesCNTRL() {
+	//var countOfWorkers int
+	//var alreadyDoneFiles int
 	
+	//keys := make([]string, 0, len(pagesWithFiles))
+    /*for k := range pagesWithFiles {
+
+
+        keys = append(keys, k)    
+    }
+
+
+*/
+	for k, v := range pagesWithFiles {
+		//go func() { 
+			fmt.Println("key", k , "value", v )
+		//}()
+	}
+	//(countOfWorkers <= WORKERS) && (alreadyDoneFiles < len(pagesWithFiles)){
+//		currentLink := 
+	
+//	}
+	
+}
+
+func DownloadFile(filepath string, url string) error {
+
+    // Get the data
+    resp, err := http.Get(url)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    // Create the file
+    out, err := os.Create(filepath)
+    if err != nil {
+        return err
+    }
+    defer out.Close()
+
+    // Write the body to file
+    _, err = io.Copy(out, resp.Body)
+    return err
+}
+
+func initial() {
+	flag.StringVar(&searchCombination,"sc", "zelda", "страница с музакальным альбомом")	
+	flag.IntVar(&WORKERS, "w", WORKERS, "количество потоков")
+
+	pagesWithFiles = make(map[string]string)
 }
 
 func main() {
-	m = make(map[string]string)
-	initCommandLineProp()
-	flag.Parse()	
 	
-	var wg sync.WaitGroup
+	initial()
+
+	flag.Parse()		
 
 	url := "https://downloads.khinsider.com/game-soundtracks/album/adk-world"
 
-		// каждый выполним параллельно
-		wg.Add(1)
-
-		// закрываем в анонимной функции переменную из цикла,
-		// что бы предотвартить её потерю во время обработки
-		go func(url string) {
-			defer wg.Done()
-			parseUrl(url)
-		}(url)
+	parseUrl(url)		
 	
-	// ждем завершения процессов
-	wg.Wait()
-	for key, value := range m {
-    	fmt.Println("Key:", key, "Value:", value)
-	}
+	downloadFilesCNTRL()
 }
 
-func initCommandLineProp(){
-	flag.StringVar(&searchCombination,"sc", "zelda", "name of the game to search")	
-}
